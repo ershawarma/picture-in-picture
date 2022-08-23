5bfc8d36050099c0f96cd79cae42b6040a5962dd
ershawarma <samuel.ssrc@gmail.com>
2022-08-22 23:56:03 -0500

    aug 22

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('ola');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play
+const selectMediaStream = async () => {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch(error) {
+        alert('something went wrong');
+    }
+}
+
+button.addEventListener('click', async () => {
+    // Disable button
+    button.disabled = true;
+    // Start picture in picture
+    await videoElement.requestPictureInPicture();
+    // Reset button
+    button.disabled = false;
+});
+
+// ON load
+selectMediaStream();
